703be2fb41f46ec2c76266572912db2bc41e2446
Luis Diaz <luisantoniodiaz97@gmail.com>
2024-07-06 15:25:26 -0600

    Prueba

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Prueba de API!!!!
+          Prueba de API!
         </a>
       </header>
     </div>
